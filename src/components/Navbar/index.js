21d05ddc990d5e36a07a87d36623fce2e89b39b2
Jhonnatan <jhonnatan133@gmail.com>
2020-04-19 19:44:52 -0500

    Finished navbar component

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import MenuIcon from '@material-ui/icons/Menu';
+import { Typography } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle'
+import ExitToAppIcon from '@material-ui/icons/ExitToApp'
+import './style.scss'
 
 const Navbar = props => {
+  const { username } = props
   return (
     <div className="af-navbarContainer">
-      Hola
+      <div className="af-left">
+        <MenuIcon className="af-icon"/>
+        <Typography variant="h6" className="af-title">
+          Prueba Front-end
+        </Typography>
+      </div>
+      <div className="af-right">
+        <AccountCircleIcon className="af-icon"/>
+        <Typography variant="subtitle2" className="af-name">
+          {username}
+        </Typography>
+        <ExitToAppIcon className="af-icon af-signOut"/>
+      </div>
     </div>
   )
 }
