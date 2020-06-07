ba5609c130437120f904a807b9012d8bd31f2827
Jhonnatan Montilla <jhonnatan133@gmail.com>
2020-06-06 22:58:09 -0500

    Added functionality to the users table.

--- src/api/data.js
+++ src/api/data.js
@@ -55,99 +55,99 @@ export const data = {
       "email": "jordanjames@enjola.com",
       "password": "prueba"
     },
-    // {
-    //   "_id": "5e9d28c758f89106d9ceeb84",
-    //   "names": "Wooten Trujillo",
-    //   "surnames": "Key Ortiz",
-    //   "national_id": "5474948352",
-    //   "role": "5e9da23b36a7e312286a1746",
-    //   "active": true,
-    //   "phone": "(984) 537-2234",
-    //   "email": "keyortiz@enjola.com",
-    //   "password": "prueba"
-    // },
-    // {
-    //   "_id": "5e9d28c7e9d2c225725bf3cb",
-    //   "names": "Olsen Petty",
-    //   "surnames": "Golden Spencer",
-    //   "national_id": "5857390228",
-    //   "role": "5e9da23ba66ce496c5805d7f",
-    //   "active": false,
-    //   "phone": "(847) 431-2430",
-    //   "email": "goldenspencer@enjola.com",
-    //   "password": "prueba"
-    // },
-    // {
-    //   "_id": "5e9d28c73d92ce2c067588f2",
-    //   "names": "Barnett Carr",
-    //   "surnames": "Staci Crawford",
-    //   "national_id": "3207657272",
-    //   "role": "5e9da23b8a03a6436ab33055",
-    //   "active": true,
-    //   "phone": "(917) 485-3428",
-    //   "email": "stacicrawford@enjola.com",
-    //   "password": "prueba"
-    // },
-    // {
-    //   "_id": "5e9e0e041cdf21da29af5981",
-    //   "names": "Estela York",
-    //   "surnames": "Acosta Lindsay",
-    //   "national_id": "2903218033",
-    //   "role": "5e9da23b9daf356a3d29587b",
-    //   "active": true,
-    //   "phone": "(913) 421-2514",
-    //   "email": "acostalindsay@enjola.com"
-    // },
-    // {
-    //   "_id": "5e9e0e0423f02b3e9e7b3ae9",
-    //   "names": "Fuller Gardner",
-    //   "surnames": "Etta Pitts",
-    //   "national_id": 4969954248,
-    //   "role": "5e9da23b36a7e312286a1746",
-    //   "active": false,
-    //   "phone": "(923) 445-3708",
-    //   "email": "ettapitts@enjola.com"
-    // },
-    // {
-    //   "_id": "5e9e0e04d7abbea4c2d7d7e8",
-    //   "names": "Gilda Huffman",
-    //   "surnames": "Todd Hampton",
-    //   "national_id": 4180414307,
-    //   "role": "5e9da23ba66ce496c5805d7f",
-    //   "active": false,
-    //   "phone": "(924) 459-2500",
-    //   "email": "toddhampton@enjola.com"
-    // },
-    // {
-    //   "_id": "5e9e0e041842124930f1ad7c",
-    //   "names": "Vivian Owens",
-    //   "surnames": "Wood Juarez",
-    //   "national_id": 4552803196,
-    //   "role": "5e9da23b8a03a6436ab33055",
-    //   "active": true,
-    //   "phone": "(869) 562-2184",
-    //   "email": "woodjuarez@enjola.com"
-    // },
-    // {
-    //   "_id": "5e9e0e042f616a7e335b976d",
-    //   "names": "Willis Franklin",
-    //   "surnames": "Clements Mooney",
-    //   "national_id": 2308546977,
-    //   "role": "5e9da23b9daf356a3d29587b",
-    //   "active": true,
-    //   "phone": "(940) 599-3639",
-    //   "email": "clementsmooney@enjola.com"
-    // },
-    // {
-    //   "_id": "5e9e0e04dd6671575721c126",
-    //   "names": "Anthony Cooke",
-    //   "surnames": "Margaret Lamb",
-    //   "national_id": 2354621318,
-    //   "role": "5e9da23b36a7e312286a1746",
-    //   "active": true,
-    //   "phone": "(825) 441-2800",
-    //   "email": "margaretlamb@enjola.com"
-    // }
+    {
+      "_id": "5e9d28c758f89106d9ceeb84",
+      "names": "Wooten Trujillo",
+      "surnames": "Key Ortiz",
+      "national_id": "5474948352",
+      "role": "5e9da23b36a7e312286a1746",
+      "active": true,
+      "phone": "(984) 537-2234",
+      "email": "keyortiz@enjola.com",
+      "password": "prueba"
+    },
+    {
+      "_id": "5e9d28c7e9d2c225725bf3cb",
+      "names": "Olsen Petty",
+      "surnames": "Golden Spencer",
+      "national_id": "5857390228",
+      "role": "5e9da23ba66ce496c5805d7f",
+      "active": false,
+      "phone": "(847) 431-2430",
+      "email": "goldenspencer@enjola.com",
+      "password": "prueba"
+    },
+    {
+      "_id": "5e9d28c73d92ce2c067588f2",
+      "names": "Barnett Carr",
+      "surnames": "Staci Crawford",
+      "national_id": "3207657272",
+      "role": "5e9da23b8a03a6436ab33055",
+      "active": true,
+      "phone": "(917) 485-3428",
+      "email": "stacicrawford@enjola.com",
+      "password": "prueba"
+    },
+    {
+      "_id": "5e9e0e041cdf21da29af5981",
+      "names": "Estela York",
+      "surnames": "Acosta Lindsay",
+      "national_id": "2903218033",
+      "role": "5e9da23b9daf356a3d29587b",
+      "active": true,
+      "phone": "(913) 421-2514",
+      "email": "acostalindsay@enjola.com"
+    },
+    {
+      "_id": "5e9e0e0423f02b3e9e7b3ae9",
+      "names": "Fuller Gardner",
+      "surnames": "Etta Pitts",
+      "national_id": 4969954248,
+      "role": "5e9da23b36a7e312286a1746",
+      "active": false,
+      "phone": "(923) 445-3708",
+      "email": "ettapitts@enjola.com"
+    },
+    {
+      "_id": "5e9e0e04d7abbea4c2d7d7e8",
+      "names": "Gilda Huffman",
+      "surnames": "Todd Hampton",
+      "national_id": 4180414307,
+      "role": "5e9da23ba66ce496c5805d7f",
+      "active": false,
+      "phone": "(924) 459-2500",
+      "email": "toddhampton@enjola.com"
+    },
+    {
+      "_id": "5e9e0e041842124930f1ad7c",
+      "names": "Vivian Owens",
+      "surnames": "Wood Juarez",
+      "national_id": 4552803196,
+      "role": "5e9da23b8a03a6436ab33055",
+      "active": true,
+      "phone": "(869) 562-2184",
+      "email": "woodjuarez@enjola.com"
+    },
+    {
+      "_id": "5e9e0e042f616a7e335b976d",
+      "names": "Willis Franklin",
+      "surnames": "Clements Mooney",
+      "national_id": 2308546977,
+      "role": "5e9da23b9daf356a3d29587b",
+      "active": true,
+      "phone": "(940) 599-3639",
+      "email": "clementsmooney@enjola.com"
+    },
+    {
+      "_id": "5e9e0e04dd6671575721c126",
+      "names": "Anthony Cooke",
+      "surnames": "Margaret Lamb",
+      "national_id": 2354621318,
+      "role": "5e9da23b36a7e312286a1746",
+      "active": true,
+      "phone": "(825) 441-2800",
+      "email": "margaretlamb@enjola.com"
+    }
   ],
   "roles": [
     {
--- src/components/UsersTable/index.js
+++ src/components/UsersTable/index.js
@@ -5,62 +5,79 @@ import { TableContainer, Table, TableHead, TableRow, TableCell,
 import EditIcon from '@material-ui/icons/Edit'
 import DeleteIcon from '@material-ui/icons/Delete'
 import { usersTableColumns } from './../../constants'
+import Aux from './../hoc/AuxFile'
 import './style.scss'
 
 const UsersTable = props => {
-  const [page, setPage] = useState(1)
   const { users, showModal, showConfirmModal } = props
+  const [page, setPage] = useState(0)
+  const [rowsPerPage, setRowsPerPage] = useState(6)
+  const [usersToShow, setUsersToShow] = useState([...users.slice(0, rowsPerPage)])
+
+  const newPageFn = (event, newPage) => {
+    setPage(newPage);
+    const usersFrom = newPage * rowsPerPage;
+    const usersTo = usersFrom + rowsPerPage;
+    setUsersToShow([...users.slice(usersFrom, usersTo)]);
+  }
+
   return (
-    <TableContainer className="af-tableContainer">
-      <Table size="small">
-        <TableHead>
-          <TableRow>
-            {usersTableColumns.map(column => 
-              <TableCell className="af-columnTitle" key={column}>
-                {column}
-              </TableCell>
-            )}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {users.map(user => 
-            <TableRow key={user._id}>
-              <TableCell>{user.names}</TableCell>
-              <TableCell>{user.surnames}</TableCell>
-              <TableCell>{user.national_id}</TableCell>
-              <TableCell>{user.role.name}</TableCell>
-              <TableCell>{user.active ? 'Activo' : 'Inactivo'}</TableCell>
-              <TableCell>{user.phone}</TableCell>
-              <TableCell>{user.email}</TableCell>
-              <TableCell>
-                <EditIcon 
-                  fontSize="small" 
-                  className="af-icon af-blue"
-                  onClick={() => showModal('Editar usuario', `${user._id}`)}
-                />
-                <DeleteIcon 
-                  fontSize="small" 
-                  className="af-icon"
-                  onClick={() => showConfirmModal(`${user._id}`)}
-                />
-              </TableCell>
+    <Aux>
+      <TableContainer className="af-tableContainer">
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              {usersTableColumns.map(column => 
+                <TableCell className="af-columnTitle" key={column}>
+                  {column}
+                </TableCell>
+              )}
             </TableRow>
-          )}
-        </TableBody>
-        {/* <TableFooter>
-          <TableRow>
-            <TablePagination
-              rowsPerPageOptions={[]}
-              rowsPerPage={8}
-              component="div"
-              count={users.length}
-              page={page}
-              onChangePage={(newPage) => setPage(newPage)}
-            />
-          </TableRow>
-        </TableFooter> */}
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {usersToShow.map(user => 
+              <TableRow key={user._id}>
+                <TableCell>{user.names}</TableCell>
+                <TableCell>{user.surnames}</TableCell>
+                <TableCell>{user.national_id}</TableCell>
+                <TableCell>{user.role.name}</TableCell>
+                <TableCell>{user.active ? 'Activo' : 'Inactivo'}</TableCell>
+                <TableCell>{user.phone}</TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell>
+                  <EditIcon 
+                    fontSize="small" 
+                    className="af-icon af-blue"
+                    onClick={() => showModal('Editar usuario', `${user._id}`)}
+                  />
+                  <DeleteIcon 
+                    fontSize="small" 
+                    className="af-icon"
+                    onClick={() => showConfirmModal(`${user._id}`)}
+                  />
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        className="af-tablePagination"
+        rowsPerPageOptions={[5, 10, 25]}
+        rowsPerPage={rowsPerPage}
+        component="div"
+        count={users.length}
+        page={page}
+        onChangePage={newPageFn}
+        backIconButtonText="Página anterior"
+        nextIconButtonText="Página siguiente"
+        labelDisplayedRows={
+          ({ from, to, count }) => {
+            return `${from}-${to} de ${count !== -1 ? count : `more than ${to}`}`
+          }
+        }
+      />
+    </Aux>
   )
 }
 
