345bcc4317fc6e10463310ed4989bd27770072a1
Jhonnatan Montilla <jhonnatan133@gmail.com>
2020-06-17 22:09:48 -0500

    Made the sidebar fixed

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Typography, Divider } from '@material-ui/core';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
@@ -8,6 +8,12 @@ import './style.scss';
 
 const Sidebar = props => {
   const { show, optSelected } = props;
+
+  useEffect(() => {
+    const element = document.querySelector(".af-sidebarContainer");
+    element.style.width = element.parentElement.offsetWidth - 14 + "px";
+  }, [show])
+
   return (
     <div className="af-sidebarContainer">
       <div className={`af-title ${show ? '' : 'contained'}`}>
